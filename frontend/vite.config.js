af216bc1d06166beee1081c25b9ab522d330544a
Rad <ellema.darrell17@gmail.com>
2025-07-05 18:48:04 +0800

    last na talaga

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,7 +1,8 @@
+// frontend/vite.config.js
 import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react-swc'
+import react from '@vitejs/plugin-react-swc' // Ito ang gusto mo!
 
-// https://vite.dev/config/
+// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
 })
